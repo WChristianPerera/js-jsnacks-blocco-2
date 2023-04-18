d79fb6f5524834ea7cfc812250e8432349a07576
WChristianPerera <christianperera13@gmail.com>
2023-04-18 19:20:04 +0200

    Add Snack3

---
+++ jsnack3/js/script.js
@@ -0,0 +1,15 @@
+// 3) snack 2.3
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+
+
+const myArray = [0,1,2,3,4,5,6,7,8,9,];
+let sum = 0;
+
+for (let i = 0; i < myArray.length; i++) {
+    if( i % 2 !== 0){
+        sum += myArray[i];
+    }
+}
+
+console.log("La somma dei numeri dispari è:" + sum);
